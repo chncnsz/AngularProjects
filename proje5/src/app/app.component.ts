edb69a7c919aaceb806982c7183b48ef4103a92e
chncnsz <ibrahim.ethem.cansiz@gmail.com>
2023-10-20 02:41:48 +0300

    Custom if - for

--- proje5/src/app/app.component.ts
+++ proje5/src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div *appCustomif="false">
+    merhaba
+  </div>
+  <!-- <ul>
+    <li *appCustomfor="5; let i = index">Deneme - {{i}}</li>
+  </ul> -->
+  <ul>
+    <li *appCustomfor="names; let name; let index = index">{{name}} - {{index + 1}}</li>
+  </ul>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'proje5';
+  names: string[] = ["Ahmet","Hasan","Yusuf","Hayri","Jardel"]
 }
--- proje5/src/app/app.module.ts
+++ proje5/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomifDirective } from './directives/customif.directive';
+import { CustomforDirective } from './directives/customfor.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomifDirective,
+    CustomforDirective
   ],
   imports: [
     BrowserModule
